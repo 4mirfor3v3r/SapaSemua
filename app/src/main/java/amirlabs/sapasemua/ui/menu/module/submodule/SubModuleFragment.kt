a504cba592f9655c19570f96240144e75038f41b
ahmad_amirudin <ahmadamirudin901@gmail.com>
2023-10-21 12:31:22 +0700

    feat: module,submodule ui

---
+++ app/src/main/java/amirlabs/sapasemua/ui/menu/module/submodule/SubModuleFragment.kt
@@ -0,0 +1,27 @@
+package amirlabs.sapasemua.ui.menu.module.submodule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import amirlabs.sapasemua.R
+import amirlabs.sapasemua.base.DevFragment
+import amirlabs.sapasemua.databinding.FragmentSubModuleBinding
+
+class SubModuleFragment : DevFragment<FragmentSubModuleBinding>(R.layout.fragment_sub_module) {
+
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+    }
+
+    override fun initAction() {
+    }
+
+    override fun initObserver() {
+    }
+
+}
