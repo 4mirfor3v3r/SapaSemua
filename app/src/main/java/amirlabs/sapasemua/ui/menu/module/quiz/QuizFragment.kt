3e1e3042c31987763af4d2fa295e6488155d5c59
ahmad_amirudin <ahmadamirudin901@gmail.com>
2023-10-21 14:27:06 +0700

    feat: quiz ui

---
+++ app/src/main/java/amirlabs/sapasemua/ui/menu/module/quiz/QuizFragment.kt
@@ -0,0 +1,25 @@
+package amirlabs.sapasemua.ui.menu.module.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import amirlabs.sapasemua.R
+import amirlabs.sapasemua.base.DevFragment
+import amirlabs.sapasemua.databinding.FragmentQuizBinding
+
+class QuizFragment : DevFragment<FragmentQuizBinding>(R.layout.fragment_quiz) {
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+    }
+
+    override fun initAction() {
+    }
+
+    override fun initObserver() {
+    }
+}
---
+++ app/src/main/java/amirlabs/sapasemua/ui/menu/module/quiz/QuizResultFragment.kt
@@ -0,0 +1,25 @@
+package amirlabs.sapasemua.ui.menu.module.quiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import amirlabs.sapasemua.R
+import amirlabs.sapasemua.base.DevFragment
+import amirlabs.sapasemua.databinding.FragmentQuizResultBinding
+
+class QuizResultFragment : DevFragment<FragmentQuizResultBinding>(R.layout.fragment_quiz_result) {
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+    }
+
+    override fun initAction() {
+    }
+
+    override fun initObserver() {
+    }
+}
