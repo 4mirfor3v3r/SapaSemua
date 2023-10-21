38049bc2db1bc86d81b09b028465443a3167494d
ahmad_amirudin <ahmadamirudin901@gmail.com>
2023-10-21 12:59:00 +0700

    feat: lesson ui

---
+++ app/src/main/java/amirlabs/sapasemua/ui/menu/module/lesson/LessonFragment.kt
@@ -0,0 +1,25 @@
+package amirlabs.sapasemua.ui.menu.module.lesson
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import amirlabs.sapasemua.R
+import amirlabs.sapasemua.base.DevFragment
+import amirlabs.sapasemua.databinding.FragmentLessonBinding
+
+class LessonFragment : DevFragment<FragmentLessonBinding>(R.layout.fragment_lesson) {
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+    }
+
+    override fun initAction() {
+    }
+
+    override fun initObserver() {
+    }
+}
